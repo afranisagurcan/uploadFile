afd5c46f337d2ad7dd90f90b41844090571c0c9e
afranisagurcan <agurcan@etu.edu.tr>
2022-06-06 10:40:27 +0300

    First commit

---
+++ src/main/java/com/example/uploadFile/repository/FileDBRepository.java
@@ -0,0 +1,8 @@
+package com.example.uploadFile.repository;
+import com.example.uploadFile.model.FileDB;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FileDBRepository extends JpaRepository<FileDB, String> {
+}
